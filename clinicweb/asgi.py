d2b442bb423a2239177488a5729784db3f4cd216
samir35181 <67304541+samir35181@users.noreply.github.com>
2021-08-12 18:35:43 -0500

    Add files via upload

---
+++ clinicweb/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for clinicweb project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinicweb.settings')
+
+application = get_asgi_application()
