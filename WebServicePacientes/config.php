617d1374a02193211645d53d844fc186769af4f1
samir35181 <67304541+samir35181@users.noreply.github.com>
2021-08-12 17:29:58 -0500

    Add files via upload

---
+++ WebServicePacientes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$db = [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'db' => 'id16620847_integradora9' //Cambiar al nombre de tu base de datos
+];
+
+
+?>
